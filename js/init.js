bd8534e5e432211477327405abc6d8ed81c6c95d
Nathan Duarte <nduartech101@gmail.com>
2016-04-07 23:09:09 -0400

    Minified stuff

--- js/init.js
+++ js/init.js
@@ -1,86 +1,66 @@
-(function ($) {
-    $(function () {
-
-        $('.button-collapse').sideNav();
-        $('.parallax').parallax();
-        $('.scrollspy').scrollSpy();
-
-    }); // end of document ready
-})(jQuery); // end of jQuery name space
-
-
-$('.button-collapse').sideNav({
-    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-});
-
-
-window.onblur = function () {
-    document.title = "Hey! Come back!";
-}
-window.onfocus = function () {
-    document.title = "IncubateX";
-}
-
-var svgOne = new Vivus('svgOne', {
-        type: 'async',
-        duration: 10,
-        dashGap: 2,
-        forceRender: false
-    }),
-    svgTwo = new Vivus('svgTwo', {
-        type: 'async',
-        duration: 10,
-        dashGap: 2,
-        forceRender: false
-    }),
-    svgThree = new Vivus('svgThree', {
-        type: 'async',
-        duration: 10,
-        dashGap: 2,
-        forceRender: false
-    });
-
-var easingFn = function (t, b, c, d) {
-    var ts = (t /= d) * t;
-    var tc = ts * t;
-    return b + c * (tc + -3 * ts + 3 * t);
-}
-var options = {  
-    useEasing: true,
-      useGrouping: false,
-      separator: ',',
-      decimal: '.',
-      prefix: '',
-      suffix: ''
-};
-var special = {  
-    useEasing: true,
-      useGrouping: false,
-      separator: ',',
-      decimal: '.',
-      prefix: '',
-      suffix: '+'
-};
-
-
 function countUp() {
-    var demo = new CountUp("counter1", 0, 3000, 0, 3, special);
-    demo.start();
-    var demo2 = new CountUp("counter2", 0, 16, 0, 3, options);
-    demo2.start();
-    var demo3 = new CountUp("counter3", 0, 30, 0, 3, options);
-    demo3.start();
-    var demo4 = new CountUp("counter4", 0, 1, 0, 3, options);
-    demo4.start();
-}
-
+    var e = new CountUp("counter1", 0, 3e3, 0, 3, special);
+    e.start();
+    var n = new CountUp("counter2", 0, 16, 0, 3, options);
+    n.start();
+    var o = new CountUp("counter3", 0, 30, 0, 3, options);
+    o.start();
+    var t = new CountUp("counter4", 0, 1, 0, 3, options);
+    t.start()
+}! function (e) {
+    e(function () {
+        e(".button-collapse").sideNav(), e(".parallax").parallax(), e(".scrollspy").scrollSpy()
+    })
+}(jQuery), $(".button-collapse").sideNav({
+    closeOnClick: !0
+}), window.onblur = function () {
+    document.title = "Hey! Come back!"
+}, window.onfocus = function () {
+    document.title = "IncubateX"
+};
+var svgOne = new Vivus("svgOne", {
+        type: "async",
+        duration: 10,
+        dashGap: 2,
+        forceRender: !1
+    }),
+    svgTwo = new Vivus("svgTwo", {
+        type: "async",
+        duration: 10,
+        dashGap: 2,
+        forceRender: !1
+    }),
+    svgThree = new Vivus("svgThree", {
+        type: "async",
+        duration: 10,
+        dashGap: 2,
+        forceRender: !1
+    }),
+    easingFn = function (e, n, o, t) {
+        var a = (e /= t) * e,
+            s = a * e;
+        return n + o * (s + -3 * a + 3 * e)
+    },
+    options = {
+        useEasing: !0,
+        useGrouping: !1,
+        separator: ",",
+        decimal: ".",
+        prefix: "",
+        suffix: ""
+    },
+    special = {
+        useEasing: !0,
+        useGrouping: !1,
+        separator: ",",
+        decimal: ".",
+        prefix: "",
+        suffix: "+"
+    };
 countUp();
-
-var stuff = [
-    {
-        selector: '#counter1',
-        offset: 0,
-        callback: 'countUp()'
-    }
-  ];
+var stuff = [{
+    selector: "#counter1",
+    offset: 0,
+    callback: "countUp()"
+}];
 Materialize.scrollFire(stuff);
